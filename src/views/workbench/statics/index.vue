84831a3edfa123bbb00a4070fbd84bd4ea1acaf2
wy <1547025615@qq.com>
2023-06-27 01:06:03 +0800

    feat: add statics module

--- src/views/workbench/statics/index.vue
+++ src/views/workbench/statics/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <el-card>统计页面</el-card>
+  <CartList></CartList>
+  <SalesTable></SalesTable>
+  <Resources></Resources>
+  <HotSearch></HotSearch>
 </template>
-<script setup lang="ts"></script>
 
-<style lang="scss" scoped></style>
+<script setup lang="ts">
+import CartList from './components/CartList/index.vue'
+import SalesTable from './components/SalesTable/index.vue'
+import Resources from './components/Resources/index.vue'
+import HotSearch from './components/HotSearch/index.vue'
+</script>
+
+<style scoped></style>
